765cfe0159b87fc534bae6542b8987ea2f02a1b8
msakho <mbsakho@yahoo.fr>
2019-10-05 18:58:33 +0200

    Update configure-standalone.sh

--- scripts/configure-standalone.sh
+++ scripts/configure-standalone.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo "Executing actions.cli"
+echo "Executing standalone-configuration.cli from custom scripts"
 /wildfly/bin/jboss-cli.sh --file=/wildfly/custom-scripts/standalone-configuration.cli
