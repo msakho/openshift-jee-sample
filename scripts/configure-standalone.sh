efb8b9c67f9b2e60553126ebc93390116567e21e
msakho <mbsakho@yahoo.fr>
2019-10-06 23:14:38 +0200

    Update configure-standalone.sh

--- scripts/configure-standalone.sh
+++ scripts/configure-standalone.sh
@@ -2,5 +2,6 @@
 echo "Executing standalone-configuration.cli from custom scripts"
 /wildfly/bin/jboss-cli.sh --file=/wildfly/custom-scripts/standalone-configuration.cli
 rm -rf /wildfly/standalone/configuration/standalone_xml_history
+/wildfly/bin/add-user.sh -a --silent=true nbservice-user nbservice-password
 
 
